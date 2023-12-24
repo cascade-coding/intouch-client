46d635348e0e96b059bc94ccaf8ca24e1eec1bcd
fahim <fahim.x@yahoo.com>
2023-12-25 04:15:43 +0600

    chore: Add an optional sm size in avatar.tsx

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,12 +1,13 @@
- 
 import React from "react";
 
 const Avatar = ({
   profile_photo,
   username,
+  sm = false,
 }: {
   profile_photo: string;
   username: string;
+  sm?: boolean;
 }) => {
   return (
     <>
@@ -16,10 +17,16 @@ const Avatar = ({
           alt="profile"
           width={28}
           height={28}
-          className="rounded-full w-8 h-8 object-cover ring-2 ring-primary"
+          className={`rounded-full ${
+            sm ? "w-5 h-5" : "w-8 h-8"
+          } object-cover ring-2 ring-primary`}
         />
       ) : (
-        <div className="bg-secondary text-secondary-foreground w-8 h-8 rounded-full text-sm flex items-center justify-center focus:outline-none cursor-pointer uppercase ring-2 ring-primary">
+        <div
+          className={`bg-secondary text-secondary-foreground ${
+            sm ? "w-5 h-5" : "w-8 h-8"
+          } rounded-full text-sm flex items-center justify-center focus:outline-none cursor-pointer uppercase ring-2 ring-primary`}
+        >
           {username.slice(0, 1)}
         </div>
       )}
